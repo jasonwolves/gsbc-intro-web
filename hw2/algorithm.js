88bf4994fc261cdf7c8e91d9f999d35acf2c1065
Jason Scott <jasonwolves@icloud.com>
2022-04-01 08:50:13 -0700

    made changed

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -6,11 +6,5 @@ var points = new Array(100);
         for (var i = 0; i < points.length; i++) {
             console.log(points[i]);
         }
-        
-        function getRandom(max)
-        {
-          
-        return Math.floor(Math.random()* max);
-            
-        }
+       
         
