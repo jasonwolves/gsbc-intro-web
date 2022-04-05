7e0e2dafdb3f10d07b580644666c6dcecb5f6714
Jason Scott <jasonwolves@icloud.com>
2022-04-05 16:21:05 -0700

    added code

--- hw2/algorithm.js
+++ hw2/algorithm.js
@@ -1,10 +1,33 @@
-var points = new Array(100);
-        for (var i = 1; i < 1000; i++) {
-            points[i] = i + 1;
-        }
+const numbers = [];
 
-        for (var i = 0; i < points.length; i++) {
-            console.log(points[i]);
-        }
-       
-        
+for (let i = 0; i <= 100; i++) {
+  numbers[i] = Math.floor(Math.random() * 1000);
+}
+//Formats the numbers
+let nLen = numbers.length;
+let text = "";
+
+for (let i = 0; i < nLen; i++) {
+  text += numbers[i] + ", ";
+
+    if (i == 100) {
+      text += numbers[i] + ", and ";
+      }
+    if (i == 100) {
+      text += numbers[i] + ".";
+      }
+  }
+document.getElementById("numbers").innerHTML = text;
+
+//Sorts the numbers and pops the largest one
+numbers.sort(function (a, b) {return a - b;});
+document.getElementById("largest_number").innerHTML = numbers.pop();
+
+//Adds all the numbers together
+let s = 0;
+let nmLen = numbers.length;
+
+for (let i = 0; i < nmLen; i++) {
+  s = s + numbers[i];
+  }
+document.getElementById("sum").innerHTML = s;
